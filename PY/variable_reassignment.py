06afebb4f604f5e77f3f956ed4761cf4cf192041
mazen-py <mazenkue@gmail.com>
2026-04-13 23:18:03 +0200

    Add variable reassignment demonstration
    A foundational script showing how variables can be initialized and later reassigned new values in Python.

---
+++ PY/variable_reassignment.py
@@ -0,0 +1,9 @@
+"""
+Program Name: Variable Reassignment
+Description: A basic demonstration of how variables hold data and can be reassigned.
+Concepts used: Variable Assignment.
+"""
+n = 5
+m = 30
+print("Type a number:")
+n = 3 # Reassigning the value of n
