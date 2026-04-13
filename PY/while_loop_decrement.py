b2b64b186309cceb1e15e5c048876cff460d7edd
mazen-py <mazenkue@gmail.com>
2026-04-13 23:21:04 +0200

    Add while loop decrement example

---
+++ PY/while_loop_decrement.py
@@ -0,0 +1,10 @@
+"""
+Program Name: While Loop Decrement
+Description: A while loop that decreases a variable by 10 each iteration.
+Concepts used: While Loops and Variable Decrementation.
+"""
+m = 50
+while m > 10:
+    print("Current value:", m)
+    m = m - 10
+print("Well done!")
