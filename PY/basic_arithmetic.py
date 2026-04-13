6bcd6fc7c9d9921a04ba164d4d623a9f948fa4b7
mazen-py <mazenkue@gmail.com>
2026-04-13 23:17:08 +0200

    Add basic arithmetic operations example
    Created a simple script demonstrating variable declaration, basic addition, and subtraction, along with a standard print statement.

---
+++ PY/basic_arithmetic.py
@@ -0,0 +1,10 @@
+"""
+Program Name: Basic Arithmetic Operations
+Description: Demonstrates simple variable assignment and math operations.
+Concepts used: Variables, Addition, and Subtraction.
+"""
+z = 2
+h = 4
+f = 5
+
+print(z + f - h)
