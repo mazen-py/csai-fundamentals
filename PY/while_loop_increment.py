5c2e70c33ca10be73545030c7a4d4082ec59ec43
mazen-py <mazenkue@gmail.com>
2026-04-13 23:20:44 +0200

    Add while loop incrementation example

---
+++ PY/while_loop_increment.py
@@ -0,0 +1,10 @@
+"""
+Program Name: While Loop Incrementation
+Description: A basic while loop that increments a counter variable until a condition is met.
+Concepts used: While Loops and Variable Incrementation.
+"""
+m = 10
+while m < 20:
+    print("Still running...")
+    m = m + 1
+print("Finished!")
